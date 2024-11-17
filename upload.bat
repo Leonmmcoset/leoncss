f9b25fb29ea4a4fffda566c5bffc5305f4da4619
LeonMMcoset <leonmmcoset@qq.com>
2024-11-17 17:33:56 +0800

    Upload 0.0.2

--- upload.bat
+++ upload.bat
@@ -2,7 +2,7 @@
 echo ---Start upload to GitHub---
 set /p var=Please input the commit:
 git pull
-git add .gitggnore
+git add .
 git commit -m "%var%"
 git push -u origin main
 echo ---End upload to GitHub---
