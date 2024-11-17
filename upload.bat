50dabd5a866cd1e498b827d275c77747d7d843c6
LeonMMcoset <152147508+Leonmmcoset@users.noreply.github.com>
2024-11-17 17:35:01 +0800

    Delete upload.bat

--- upload.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ---Start upload to GitHub---
-set /p var=Please input the commit:
-git pull
-git add .
-git commit -m "%var%"
-git push -u origin main
-echo ---End upload to GitHub---
-pause
